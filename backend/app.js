0643a189c29e0f5d54c7fae0eb26a81f8e0fc34c
Barbara Schvuchov <schvuchov.k@gmail.com>
2024-07-30 21:50:24 -0300

    configurando dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,16 @@
+require("dotenv").config()
+
 const express = require("express")
-const path = require("path")
+const path = require("path")  //diretorio das imagens
 const cors = require("cors")
 
-const port = 5000;
+const port = process.env.PORT;
 
 const app = express()
 
 //config JSON and form data responde
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })); //esse outro formato é pq json n usa imagem
 
 app.listen(port, () => {
     console.log(`App rodando na porta ${port}`)
